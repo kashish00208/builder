6ac6952222131d9d092c3a83c3edb93af51c27a3
Kashish <kgupta7827@gmail.com>
2025-11-03 22:31:49 +0530

    minor changes

--- model/User.ts
+++ model/User.ts
@@ -9,6 +9,7 @@ interface IUser {
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: {
+    
     type: String,
     required: true,
     match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address"],
